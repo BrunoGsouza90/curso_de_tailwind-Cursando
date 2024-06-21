7d76992f976716f771dcfef33335f1d89d3c8954
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-06-21 18:00:19 -0300

    Aula 2 do Curso de TailWind CSS

---
+++ Aulas/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "C:\\Users\\conta\\Desktop\\Cursos\\curso_de_tailwind\\Aulas\\**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
