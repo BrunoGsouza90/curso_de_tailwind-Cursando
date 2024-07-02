88033f1364863b9ea45eef80548c2d62fb46cfe0
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-07-01 22:21:42 -0300

    Aula 13

---
+++ Aulas/SRC/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+
+module.exports = {
+  darkMode: 'class', // Habilita o modo escuro com base na classe HTML
+  theme: {
+    extend: {
+      colors: {
+        // Defina suas cores personalizadas aqui, se necessário
+        // Por exemplo, para uma cor de fundo escura
+        dark: '#374151',
+      },
+    },
+  },
+  plugins: [],
+};
